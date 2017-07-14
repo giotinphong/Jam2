a91dde9159dfe79e4341fbc141e69d936eb75447
giotinphong <giotinphong001@gmail.com>
2017-07-14 12:57:30 +0700

    prepare to create media player layout

--- app/src/main/java/com/swings/jam/ui/TypeSonActivity.java
+++ app/src/main/java/com/swings/jam/ui/TypeSonActivity.java
@@ -1,15 +1,15 @@
 package com.swings.jam.ui;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.swings.jam.R;
 import com.swings.jam.adapter.typeson.TypeSonAdapter;
@@ -17,13 +17,14 @@ import com.swings.jam.data.DataHelper;
 import com.swings.jam.data.TypeSon;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public class TypeSonActivity2 extends AppCompatActivity {
+public class TypeSonActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_type_son2);
+        setContentView(R.layout.activity_type_son);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -31,11 +32,24 @@ public class TypeSonActivity2 extends AppCompatActivity {
         int typeId = getIntent().getExtras().getInt("typeId");
         ArrayList<TypeSon> typeSonArrayList = dataHelper.selectAllTypeSonByIdType(typeId);
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.acti_typeson_recyclerview);
-        TypeSonAdapter adapter = new TypeSonAdapter(typeSonArrayList,TypeSonActivity2.this);
-        LinearLayoutManager layoutManager1 = new LinearLayoutManager(TypeSonActivity2.this);
+        TypeSonAdapter adapter = new TypeSonAdapter(typeSonArrayList,TypeSonActivity.this);
+        LinearLayoutManager layoutManager1 = new LinearLayoutManager(TypeSonActivity.this);
         layoutManager1.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(layoutManager1);
+
+
+        //change
+        ArrayList<Integer> drawableList = new ArrayList<>();
+        for(TypeSon typeSon : typeSonArrayList){
+            drawableList.add(typeSon.getImage());
+        }
+        Random ran = new Random();
+        int numran = ran.nextInt(drawableList.size());
+        Drawable drawable = this.getResources().getDrawable(drawableList.get(numran));
+        CollapsingToolbarLayout toolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.toolbar_layout);
+        toolbarLayout.setBackground(drawable);
+        toolbarLayout.setTitle(getIntent().getExtras().getString("typeName"));
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
